c157e8062d358c4941dd9fd54b646175ab4e77db
xKOOOTx <kooot123@gmail.com>
2021-07-05 00:59:29 +0300

    hot fix

--- js/profile.js
+++ js/profile.js
@@ -2,7 +2,9 @@ const profileBtn = document.querySelector('.profile');
 const aside = document.querySelector('.aside')
 const body = document.body
 
-profileBtn.addEventListener('click', () => {
-    aside.classList.toggle('mobile-active')
-    body.classList.toggle('overflowHid')
-})
+if (document.documentElement.clientWidth < 800) {
+    profileBtn.addEventListener('click', () => {
+        aside.classList.toggle('mobile-active')
+        body.classList.toggle('overflowHid')
+    })
+}
